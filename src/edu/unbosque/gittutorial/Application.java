4ebd1dc42d60c9a2df4e29a3c176f682c68670f5
Fabian Peña <fabiancpl91@gmail.com>
2022-02-11 12:10:25 -0500

    Extending for requesting to user the student name

--- src/edu/unbosque/gittutorial/Application.java
+++ src/edu/unbosque/gittutorial/Application.java
@@ -1,9 +1,29 @@
 package edu.unbosque.gittutorial;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Bienvenido estudiante");
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader (isr);
+
+        try {
+            System.out.print("Ingrese el nombre del estudiante: ");
+            String name = br.readLine();
+
+            Student student = new Student();
+            student.setName(name);
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
 }
